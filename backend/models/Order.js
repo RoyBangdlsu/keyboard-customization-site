821317510e4fd845e97c8af21c670b0233e0f384
Raphael Karlo Santiago <rappykarlopi@gmail.com>
2025-02-24 19:40:48 +0800

    Order Backend updated!

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
 
 const OrderNewSchema = new mongoose.Schema({
-  customerName: { type: String, required: true },
-  keyboardType: { type: String, required: true },
-  price: { type: Number, required: true },
+  serviceType: { type: String, required: true},
+  keyboardSize: { type: String, required: true },
+  keycapBrand: { type: String, required: true },
+  swtichType: { type: String, required: true },
+  price: { type: Number, required: true }
 }, { timestamps: true });
 
 export default mongoose.model("Order", OrderNewSchema);
