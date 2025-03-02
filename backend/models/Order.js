b1036b85e0406860e96e33a2440bc253b890da83
rappykarlopi <107909594+rappykarlopi@users.noreply.github.com>
2025-03-02 09:29:07 +0800

    Update Order.js

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -3,15 +3,17 @@ import mongoose from "mongoose";
 const OrderSchema = new mongoose.Schema({
   customerName: { type: String, required: true }, 
   customerEmail: { type: String, required: true }, 
+  address: { type: String, required: true },
   serviceType: { type: String, required: true },
   keyboardSize: { type: String, required: true },
   keycapBrand: { type: String, required: true },
   switchType: { type: String, required: true },
-  switchLubing: { type: Number, required: true },
-  filming: { type: Number, required: true },
-  stabilizers: { type: Number, required: true },
-  tapeLayers: { type: Number, required: true },
   price: { type: Number, required: true },
+  orderStatus: { type: String, required: true },
+  keyboardImage: { 
+    data: Buffer, // Binary data for the image
+    contentType: String // MIME type of the image (e.g., 'image/jpeg')
+  }
 }, { timestamps: true });
 
 export default mongoose.model("Order", OrderSchema);
