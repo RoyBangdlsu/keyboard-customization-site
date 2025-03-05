5a9796076222f28d4eaf37fee19238b1e542ce21
Ian Sianghio <adrieanne_sianghio@dlsu.edu.ph>
2025-03-05 20:53:12 +0800

    updated aboutus

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -6,7 +6,21 @@ function About() {
     <div className="about-container">
       <div className="about-content">
         <h1>About Us</h1>
-        <p>We are building a custom keyboard website.</p>
+        <p className="justified">
+          Hi, I'm Coby—the team behind Cobs Keebs! I'm a one-man team, handling everything from picking parts and recommending boards to modifying and building them. I'm just a university student who happens to love building keyboards.
+        </p>
+        <p className="justified">
+          Thank you to all the people who have supported me and my small passion project of a business! To all my friends and family who continue to give unprecedented amounts of support and to all my clients—I couldn't have made it this far without you. Thank you for entrusting me with building your dream boards, and here's to building many more in the future!
+        </p>
+      </div>
+      <div className="below-section">
+        <div className="image-wrapper">
+          <img
+            src="https://i.imgur.com/ufYIf61.jpeg"
+            alt="Keyboard Below"
+            className="below-image"
+          />
+        </div>
       </div>
     </div>
   );
