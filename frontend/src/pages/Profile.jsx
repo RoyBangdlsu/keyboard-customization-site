c149fe51eb8cc5e3129514599863f1ce56adc9bc
Raphael Karlo Santiago <rappykarlopi@gmail.com>
2025-04-03 14:48:05 +0800

    profile fix

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -307,27 +307,27 @@ function Profile() {
                                             <tbody>
                                               <tr>
                                                 <td>Keyboard Size: {order.keyboardSize}</td>
-                                                <td className="alignright">₱{order.keyboardSizePrice ? parseFloat(order.keyboardSizePrice).toFixed(2) : "0.00"}</td>
+                                                <td className="alignright">₱{order.keyboardSizePrice.toFixed(2)}</td>
                                               </tr>
                                               <tr>
                                                 <td>Keycap Brand: {order.keycapBrand}</td>
-                                                <td className="alignright">₱{order.keyCapBrandPrice ? parseFloat(order.keyCapBrandPrice).toFixed(2) : "0.00"}</td>
+                                                <td className="alignright">₱{order.keyCapBrandPrice.toFixed(2)}</td>
                                               </tr>
                                               <tr>
                                                 <td>Switch Type: {order.switchType}</td>
-                                                <td className="alignright">₱{order.switchTypePrice ? parseFloat(order.switchTypePrice).toFixed(2) : "0.00"}</td>
+                                                <td className="alignright">₱{order.switchTypePrice.toFixed(2)}</td>
                                               </tr>
                                               <tr>
                                                 <td>Order Status:</td>
                                                 <td className="alignright">
                                                   <span className={`order-status ${order.status ? order.status.toLowerCase() : ''}`}>
-                                                    {order.status || 'Waiting For Review'}
+                                                    {order.status}
                                                   </span>
                                                 </td>
                                               </tr>
                                               <tr className="total">
                                                 <td className="alignright" width="80%">Total</td>
-                                                <td className="alignright">₱{order.total ? parseFloat(order.total).toFixed(2) : "0.00"}</td>
+                                                <td className="alignright">₱{order.total.toFixed(2)}</td>
                                               </tr>
                                             </tbody>
                                           </table>
